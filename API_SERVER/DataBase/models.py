b4eda6599c41c0ab8d2587b6678dab1477dc5fa1
agg4565 <agg4565@gmail.com>
2025-02-03 15:48:34 +0900

    models.py 수정

--- API_SERVER/DataBase/models.py
+++ API_SERVER/DataBase/models.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 # 데이터베이스 연결 및 테이블 생성
 
 
+
 class TB_USERS(Base):
     __tablename__ = "TB_USERS"
 
@@ -21,22 +22,10 @@ class TB_USERS(Base):
     UPDATED_AT = Column(TIMESTAMP, comment="수정 일시")
 
 
-class TB_CROOM(Base):
-    __tablename__ = "TB_CROOM"
-
-    CROOM_IDX = Column(Integer, nullable=False, primary_key=True, autoincrement=True, comment="방 고유번호")
-    CROOM_TITLE = Column(String(1000), comment="방 제목")
-    CROOM_INFO = Column(Text, comment="방 소개")
-    USER_ID = Column(String(50), comment="방 개설자")
-    CROOM_LIMIT = Column(Integer, default=0, comment="방 인원수")
-    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="방 개설일자")
-    CROOM_STATUS = Column(String(10), comment="방 상태")
-
-
 class TB_POI(Base):
     __tablename__ = "TB_POI"
 
-    POI_IDX = Column(Integer, nullable=False, primary_key=True, autoincrement=True, comment="여행지 고유번호")
+    POI_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="여행지 고유번호")
     POI_NM = Column(String(255), comment="여행지 명")
     POI_INFO = Column(Text, comment="여행지 설명")
     POI_ADDR = Column(String(1000), comment="여행지 주소")
@@ -44,54 +33,27 @@ class TB_POI(Base):
     POI_REGION = Column(String(100), comment="지역")
     POI_TEL = Column(String(20), comment="여행지 전화번호")
     POI_PERIOD = Column(String(1000), comment="이용 시간")
-    LAT = Column(DECIMAL(17, 14), default=0.0, comment="위도")
-    LON = Column(DECIMAL(17, 14), default=0.0, comment="경도")
-    POI_LIKES = Column(Integer, default=0, comment="좋아요 수")
+    LAT = Column(DECIMAL(17, 14), server_default="0.00000000000000", comment="위도")
+    LON = Column(DECIMAL(17, 14), server_default="0.00000000000000", comment="경도")
+    POI_LIKES = Column(Integer, server_default="0", comment="좋아요 수")
     CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="생성 일시")
     UPDATED_AT = Column(TIMESTAMP, comment="수정 일시")
 
 
-class TB_SHOPPING_MALL(Base):
-    __tablename__ = "TB_SHOPPING_MALL"
+class TB_POI_RECO(Base):
+    __tablename__ = "TB_POI_RECO"
 
-    MALL_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="몰 고유번호")
-    CATEGORY = Column(String(100), comment="카테고리 명")
-    MALL_NM = Column(String(255), comment="쇼핑몰 이름")
-    MALL_URL = Column(String(1000), comment="쇼핑몰 URL")
-    MALL_IMG = Column(String(1000), comment="쇼핑몰 이미지")
-    MALL_LIKES = Column(Integer, default=0, comment="쇼핑몰 좋아요수")
-    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="생성 일시")
-    UPDATED_AT = Column(TIMESTAMP, comment="수정 일시")
-
-
-class TB_CHATTING(Base):
-    __tablename__ = "TB_CHATTING"
-
-    CHAT_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="채팅 고유번호")
-    CROOM_IDX = Column(Integer, comment="방 고유번호")
-    USER_ID = Column(String(50), comment="발화자")
-    CHAT_CONTENT = Column(Text, comment="챗봇 응답 내용")
-    USER_CONTENT = Column(Text, comment="유저 발화 내용")
-    CHAT_FILE = Column(String(1000), comment="발화 첨부파일")
-    CHAT_EMOTION = Column(String(1000), comment="발화 이모티콘")
-    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="발화 시간")
-
-
-class TB_REVIEW(Base):
-    __tablename__ = "TB_REVIEW"
-
-    REVIEW_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="후기 식별자")
-    POI_IDX = Column(Integer, comment="여행지 고유번호")
+    RECO_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="여추 고유번호")
     USER_ID = Column(String(50), comment="사용자 아이디")
-    REVIEW_CONTENT = Column(Text, comment="후기 내용")
-    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="생성 일시")
-    UPDATED_AT = Column(TIMESTAMP, comment="수정 일시")
+    POI_IDX = Column(Integer, comment="여행지 고유번호")
+    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="추천 일시")
+    RECO_REASON = Column(Text, comment="추천 사유")
 
 
 class TB_SCHEDULE(Base):
     __tablename__ = "TB_SCHEDULE"
 
-    SCHE_IDX = Column(String(60),nullable=False, primary_key=True, comment="일정 고유번호")
+    SCHE_IDX = Column(String(60), primary_key=True, comment="일정 고유번호")
     TOUR_NM = Column(String(255), comment="여행 명")
     TOUR_TYPE = Column(String(50), comment="여행 형태")
     TOUR_DESC = Column(Text, comment="여행 소개")
@@ -105,65 +67,54 @@ class TB_SCHEDULE(Base):
 class TB_TIMETABLE(Base):
     __tablename__ = "TB_TIMETABLE"
 
-    TT_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="세부 고유번호")
+    TT_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="세부 고유번호")
     SCHE_IDX = Column(String(60), comment="일정 고유번호")
     TT_DATE = Column(Date, comment="여행 날짜")
     ST_TIME = Column(Time, comment="여행 시작 시간")
     POI_IDX = Column(Integer, comment="여행지 고유번호")
-    TT_ORDER = Column(Integer, default=0, comment="세부 순번")
+    TT_ORDER = Column(Integer, server_default="0", comment="세부 순번")
     CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="등록 일시")
     UPDATED_AT = Column(TIMESTAMP, comment="수정 일시")
 
 
-class TB_POI_RECO(Base):
-    __tablename__ = "TB_POI_RECO"
+class TB_REVIEW(Base):
+    __tablename__ = "TB_REVIEW"
 
-    RECO_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="여추 고유번호")
-    USER_ID = Column(String(50), comment="사용자 아이디")
+    REVIEW_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="후기 식별자")
     POI_IDX = Column(Integer, comment="여행지 고유번호")
-    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="추천 일시")
-    RECO_REASON = Column(Text, comment="추천 사유")
+    USER_ID = Column(String(50), comment="사용자 아이디")
+    REVIEW_CONTENT = Column(Text, comment="후기 내용")
+    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="생성 일시")
+    UPDATED_AT = Column(TIMESTAMP, comment="수정 일시")
+
+
+class TB_SHOPPING_MALL(Base):
+    __tablename__ = "TB_SHOPPING_MALL"
+
+    MALL_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="몰 고유번호")
+    CATEGORY = Column(String(100), comment="카테고리 명")
+    MALL_NM = Column(String(255), comment="쇼핑몰 이름")
+    MALL_URL = Column(String(1000), comment="쇼핑몰 URL")
+    MALL_IMG = Column(String(1000), comment="쇼핑몰 이미지")
+    MALL_LIKES = Column(Integer, server_default="0", comment="쇼핑몰 좋아요수")
+    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="생성 일시")
+    UPDATED_AT = Column(TIMESTAMP, comment="수정 일시")
 
 
 class TB_MALL_RECO(Base):
     __tablename__ = "TB_MALL_RECO"
 
-    RECO_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="쇼추 고유번호")
+    RECO_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="쇼추 고유번호")
     CHAT_IDX = Column(Integer, comment="채팅 고유번호")
     MALL_IDX = Column(Integer, comment="몰 고유번호")
     CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="추천 일시")
     USER_ID = Column(String(50), comment="사용자 아이디")
 
 
-class TB_LINE_COMMENT(Base):
-    __tablename__ = "TB_LINE_COMMENT"
-
-    LINE_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="한줄 고유번호")
-    POI_IDX = Column(Integer, comment="여행지 고유번호")
-    LINE_CONTENT = Column(String(300), comment="한줄 내용")
-    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="한줄 작성일자")
-    USER_ID = Column(String(50), comment="한줄 작성자")
-
-
-class TB_FILE(Base):
-    __tablename__ = "TB_FILE"
-
-    FILE_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="파일 식별자")
-    POI_IDX = Column(Integer, comment="여행지 고유번호")
-    FILE_NM = Column(String(1000), comment="파일 명")
-    FILE_SIZE = Column(Integer, default=0, comment="파일 사이즈")
-    FILE_EXT = Column(String(10), comment="파일 확장자")
-    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="업로드 날짜")
-    UPDATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="수정 날짜")
-    USER_ID = Column(String(50), comment="작성자 아이디")
-    DISP_ORDER = Column(Integer, default=0, comment="표시 순서")
-    REVIEW_IDX = Column(Integer, comment="후기 식별자")
-
-
 class TB_FESTIVAL(Base):
     __tablename__ = "TB_FESTIVAL"
 
-    FEST_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="행사 식별자")
+    FEST_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="행사 식별자")
     FEST_NM = Column(String(100), comment="행사 이름")
     FEST_DESC = Column(Text, comment="행사 소개")
     FEST_ADDR = Column(String(1000), comment="행사 주소")
@@ -177,10 +128,60 @@ class TB_FESTIVAL(Base):
     UPDATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="수정 일시")
 
 
+class TB_CHATTING(Base):
+    __tablename__ = "TB_CHATTING"
+
+    CHAT_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="채팅 고유번호")
+    CROOM_IDX = Column(Integer, comment="방 고유번호")
+    USER_ID = Column(String(50), comment="발화자")
+    CHAT_CONTENT = Column(Text, comment="발화 내용")
+    CHAT_FILE = Column(String(1000), comment="발화 첨부파일")
+    CHAT_EMOTION = Column(String(1000), comment="발화 이모티콘")
+    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="발화 시간")
+    USER_CONTENT = Column(Text)
+
+
+class TB_CROOM(Base):
+    __tablename__ = "TB_CROOM"
+
+    CROOM_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="방 고유번호")
+    CROOM_TITLE = Column(String(1000), comment="방 제목")
+    CROOM_INFO = Column(Text, comment="방 소개")
+    USER_ID = Column(String(50), comment="방 개설자")
+    CROOM_LIMIT = Column(Integer, server_default="0", comment="방 인원수")
+    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="방 개설일자")
+    CROOM_STATUS = Column(String(10), comment="방 상태")
+
+
+class TB_LINE_COMMENT(Base):
+    __tablename__ = "TB_LINE_COMMENT"
+
+    LINE_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="한줄 고유번호")
+    POI_IDX = Column(Integer, comment="여행지 고유번호")
+    LINE_CONTENT = Column(String(300), comment="한줄 내용")
+    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="한줄 작성일자")
+    USER_ID = Column(String(50), comment="한줄 작성자")
+
+
+class TB_FILE(Base):
+    __tablename__ = "TB_FILE"
+
+    FILE_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="파일 식별자")
+    POI_IDX = Column(Integer, comment="여행지 고유번호")
+    FILE_NM = Column(String(1000), comment="파일 명")
+    FILE_SIZE = Column(Integer, server_default="0", comment="파일 사이즈")
+    FILE_EXT = Column(String(10), comment="파일 확장자")
+    CREATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="업로드 날짜")
+    UPDATED_AT = Column(TIMESTAMP, server_default=func.current_timestamp(), comment="수정 날짜")
+    USER_ID = Column(String(50), comment="작성자 아이디")
+    DISP_ORDER = Column(Integer, server_default="0", comment="표시 순서")
+    REVIEW_IDX = Column(Integer, comment="후기 식별자")
+
+
 class TB_LIKE(Base):
     __tablename__ = "TB_LIKE"
 
-    LIKE_IDX = Column(Integer,nullable=False, primary_key=True, autoincrement=True, comment="좋아요 식별자")
+    LIKE_IDX = Column(Integer, primary_key=True, autoincrement=True, comment="좋아요 식별자")
     USER_ID = Column(String(50), comment="사용자 아이디")
     MALL_IDX = Column(Integer, comment="몰 고유번호")
     POI_IDX = Column(Integer, comment="여행지 고유번호")
