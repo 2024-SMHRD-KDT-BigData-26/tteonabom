1cece6d013a5aa5461b1f75bffc450fb22c3275e
Dotoriberry <dotoriberry@gmail.com>
2025-02-05 08:58:51 +0900

    fix: 실제 이미지, 디폴트 이미지 제대로 뜨지 않던 문제 수정

--- frontend/src/routes/SpotView.svelte
+++ frontend/src/routes/SpotView.svelte
@@ -320,7 +320,7 @@
         </div>
         <!-- 중간: 이미지와 표 형태 정보 -->
         <div class="card d-flex flex-row align-items-start card-body-div">
-          <img src={poi.POI_URL || "../src/assets/img/default_image_o.png"} alt="여행지 이미지" class="card-img-top" on:click={toggleModal} />
+          <img src={spot?.POI_URL || "../src/assets/img/default_image_o.png"} alt="여행지 이미지" class="card-img-top" on:click={toggleModal} />
           <div class="card-body">
             <table class="table table-hover">
               <tbody>
@@ -405,7 +405,7 @@
   <!-- 이미지 확대 모달 -->
   {#if isModalOpen}
     <div class="modal" on:click={toggleModal}>
-      <img src={spot.POI_URL} alt="확대된 이미지" />
+      <img src={spot?.POI_URL || "../src/assets/img/default_image_o.png"} alt="확대된 이미지" />
     </div>
   {/if}
 </main>
