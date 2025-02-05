6d72b19786528122b3e397d61ba490c7f0a8510e
Dotoriberry <dotoriberry@gmail.com>
2025-02-05 20:56:16 +0900

    feat: 여행지, 행사축제 텍스트 검색 기능 추가

--- frontend/src/routes/SpotList.svelte
+++ frontend/src/routes/SpotList.svelte
@@ -74,7 +74,7 @@
     if (regionsChecked[region]) {
       nationwideChecked = false;
     }
-  }
+}
 
   // 페이지네이션을 위한 상태
   let currentPageNumber = 1; 
@@ -83,18 +83,34 @@
   // 기본 정렬 순서
   let sortOption = 'latest';
 
+  // 검색어 상태
+  let searchQuery = '';
+
+  // 검색 기능
+  function handleSearch() {
+    currentPageNumber = 1; // 검색 시 첫 페이지로 이동
+  }
+
   // 링크
   const goToView = (POI_IDX) => {
     link(`/SpotView/${POI_IDX}`);
   };
 
-  // 지역 필터링, 순서 정렬
+  // 지역 필터링, 순서 정렬, 검색 기능
   $: filteredSpots = pois
   .filter(spot => {
     if (nationwideChecked) return true;
     return Object.keys(regionsChecked).some(region => 
       regionsChecked[region] && spot.POI_ADDR.includes(regionNameMap[region]));
   })
+  .filter(spot => {
+  if (!searchQuery) return true;
+  return (
+    spot.POI_NM?.includes(searchQuery) ||
+    spot.POI_DESC?.includes(searchQuery) ||
+    spot.POI_ADDR?.includes(searchQuery)
+  );
+})
   .slice()
   .sort((a, b) => {
     if (sortOption === "latest" && a.CREATED_AT && b.CREATED_AT) return new Date(b.CREATED_AT) - new Date(a.CREATED_AT);
@@ -130,6 +146,15 @@
   .form-check-input:checked {
     background-color: #FF5D17;
     border-color: #FF5D17;
+  }
+
+  /* 검색창 스타일 */
+  .form-control {
+    box-shadow: none;
+    width: 200px;
+    font-size: 14px;
+    padding: 10px;
+    margin: auto;
   }
 
   /* 추천 여행지 컨테이너 */
@@ -366,13 +391,18 @@
       </div>
     </div>
     <!-- 지역 필터링 끝 -->
-    <!-- 게시물 수 / 정렬 시작-->
+    <!-- 게시물 수 / 정렬 / 검색 시작-->
     <div class="container mt-4">
       <div class="d-flex justify-content-between align-items-center">
         <div>
           <span>총 {filteredSpots.length}건</span>
         </div>
-        <div>
+        <div class="d-flex gap-2">
+          <!-- 검색창 -->
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="찾을 내용을 입력해주세요" bind:value={searchQuery}>
+          </div>
+          <!-- 정렬 -->
           <select class="form-select" style="width: 100px;" bind:value={sortOption}>
             <option value="latest">최신순</option>
             <option value="popular">인기순</option>
